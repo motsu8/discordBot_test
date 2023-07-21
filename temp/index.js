d7210cc468ab22bd5560ca3e3d7ede4ba93d4f98
motsu8e <motsu159357@gmail.com>
2023-07-22 01:50:26 +0900

    サーバーに参加しているユーザー取得

--- temp/index.js
+++ temp/index.js
@@ -2,13 +2,6 @@ const DISCORD_GUILD_ID = "1029144547571282053";
 const DISCORD_CHANNEL_ID = "1115179542164811776";
 const MESSAGE = "hogehogehoge";
 
-// curl \
-// -X POST \
-// -H "Content-Type: application/json" \
-// -H "Authorization: Bot $DISCORD_BOT_TOKEN" \
-// -d "{\"content\": \"$MESSAGE\"}" \
-// "https://discordapp.com/api/channels/$DISCORD_CHANNEL_ID/messages"
-
 // discord.jsライブラリの中から必要な設定を呼び出し、変数に保存します
 const { Client, Events, GatewayIntentBits } = require("discord.js");
 const express = require("express");
@@ -16,23 +9,27 @@ const app = express();
 
 // 設定ファイルからトークン情報を呼び出し、変数に保存します
 const { token } = require("../config.json");
+const { default: Undici } = require("undici");
 
 // クライアントインスタンスと呼ばれるオブジェクトを作成します
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
 
 // クライアントオブジェクトが準備OKとなったとき一度だけ実行されます
-client.once(Events.ClientReady, (c) => {
+client.once(Events.ClientReady, async (c) => {
   console.log(`準備OKです! ${c.user.tag}がログインします。`);
 
   // // サーバーのテキストチャンネルリストを取得
-  // console.log("---サーバーのテキストチャンネルリスト---");
-  // const guild = client.guilds.cache.get(DISCORD_GUILD_ID);
-  // console.log(guild.members.cache);
-  // const textChannels = guild.channels.cache.filter(
-  //   (channel) => channel.type === 0
-  // );
-  // textChannels.each((channel) => console.log(channel.name));
-  // console.log("------");
+  console.log("---サーバーメンバー---");
+  const guild = await client.guilds.fetch(DISCORD_GUILD_ID);
+  console.log(guild);
+  const members = await guild.members.list({limit: 50, cache: true})
+  members.each(member => console.log(member.user.username))
 
   // // サーバーの特定のテキストチャンネルに参加しているメンバー取得
   // const members = textChannels.first().members;
@@ -45,21 +42,50 @@ client.once(Events.ClientReady, (c) => {
   // member.roles.cache.has('役職ID')
 });
 
+client.on("messageCreate", async (message) => {
+  console.log(message);
+  if (message.content === "!getUsers") {
+    // サーバーのテキストチャンネルを取得
+    const textChannels = client.channels.cache.filter(
+      (channel) => channel.type === "text"
+    );
+
+    // テキストチャンネルごとにユーザーリストを取得
+    textChannels.forEach(async (channel) => {
+      const guild = channel.guild;
+      const memberList = guild.members.cache.filter(
+        (member) => !member.user.bot
+      );
+
+      // ユーザーリストをコンソールに表示（ここでは表示のみですが、適宜他の処理を行うこともできます）
+      console.log(`チャンネル「${channel.name}」のユーザーリスト:`);
+      memberList.forEach((member) => {
+        console.log(`- ${member.user.username}`);
+      });
+    });
+  }
+});
+
 app.use(express.json());
 
+function createCodeBlock(code) {
+  return "```" + code + "```";
+}
+
 app.post("/send-message", (req, res) => {
-  const message = req.body.message;
-  console.log(req.body)
+  const message = `${req.body.message} \n ${createCodeBlock(
+    'console.log("ok")'
+  )}`;
+  console.log(req.body);
   console.log(`Received message: ${message}`);
 
   // ここでメッセージを処理するための任意の操作を実行できます
   const guild = client.guilds.cache.get(DISCORD_GUILD_ID);
   const channel = guild.channels.cache.get(DISCORD_CHANNEL_ID);
   if (!channel) return res.status(400).json({ error: "Invalid channel ID." });
-  res.send("Message received successfully.");
+  res.status(200).json({ message: "Message sent successfully." });
 
   channel.send(message);
-  res.status(200).json({ message: "Message sent successfully." });
 });
 
 // 静的ファイルの提供
@@ -72,7 +98,9 @@ app.get("/", (req, res) => {
 
 const PORT = 3030;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT} url: http://localhost:${PORT}`);
+  console.log(
+    `Server is running on port ${PORT} url: http://localhost:${PORT}`
+  );
 });
 
 // ログインします
